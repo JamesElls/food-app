223f9222918ec34d68b66b42f17892a45f56dddb
Keyshaonline <keyshaleonidas01@gmail.com>
2021-03-09 16:44:41 -0700

    navbar

---
+++ client/src/navbar.js
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom'
+import { Menu } from 'semantic-ui-react'
+
+const navBar = ()=> {
+return(
+<Menu> 
+  <Link to='/'>
+  <Menu.Item>
+                    foods
+                </Menu.Item>
+            </Link>
+            <Link to='/foods'>
+                <Menu.Item>
+                    cooks
+              </Menu.Item>
+            </Link>
+            <Link to='/cooks'>
+              <Menu.Item>
+              </Menu.Item>
+            </Link>              
+
+     
+     
+</Menu>
+
+)
+
+}
