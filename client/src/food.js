bf68693bf2512d4f9f2ed3194589db77c2cc080e
Max Manayan <maximillian521@gmail.com>
2021-03-09 17:41:19 -0700

    js components imported and functional

--- client/src/food.js
+++ client/src/food.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const Food = () => {
   return(
     <div>
@@ -13,16 +13,3 @@ const Food = () => {
 
 
 export default Food;
-=======
-import React from 'react'
-
-const food = () => {
-    return (
-        <div>
-            <h1>Food + restaurants here</h1>
-        </div>
-    )
-}
-
-export default food
->>>>>>> 6bfdd6913175dac4dba367f776ac117ede1a9151
