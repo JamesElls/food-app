dc8de48b03e61a252227bfc1091d2f1dc18b1c2d
jamesElls <jamellsworth@gmail.com>
2021-03-10 14:40:23 -0700

    Food homepage

--- client/src/food.js
+++ client/src/food.js
@@ -1,15 +1,18 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import { Card , Button, CardHeader} from "semantic-ui-react"
+import { useParams, useHistory, Link } from 'react-router-dom'
+import Restaurant from './restaurant'
 
 const Food = () => {
+    const { id } = useParams()
+    
+
   return(
     <div>
-      <h1>Food Page</h1>
+      <h1>Please work</h1>
     </div>
   )
 }
-
-
-
-
-
 
 export default Food;
--- client/src/foods.js
+++ client/src/foods.js
@@ -1,10 +1,14 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
-import food from './food'
+import { Card , Button, CardHeader} from "semantic-ui-react"
+import { Link } from 'react-router-dom'
+
 
 const Foods = () => {
 
     const [foods, setFoods] =useState([])
+    
+    
 
     useEffect(() => {
         getFoods()
@@ -25,7 +29,18 @@ const Foods = () => {
         }
         return foods.map( food =>{
             return(
-             <h3>check back soon for this feature</h3>
+                // <Card>
+                //     <Card.Content>
+                //         <Card.Header>{food.name}</Card.Header>
+                //     </Card.Content>
+                // </Card>
+                <button>          
+                <Link to={`/food/${food.id}`}>
+                    <div>
+                        {food.name}
+                    </div>
+                </Link>
+              </button>
             )
         })
     }
@@ -33,7 +48,7 @@ const Foods = () => {
     console.log(foods)
 
     return (
-        <div>
+        <div className='food-homepage'>
             {renderFoods()}
         </div>
     )
