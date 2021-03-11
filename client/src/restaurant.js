d4764a51846b25a55065c8f11e16c5c2ed68b7ca
Santiago Lozano Ventura <santiago.ventura2012@gmail.com>
2021-03-11 15:14:45 -0700

    restaurant delete working

--- client/src/restaurant.js
+++ client/src/restaurant.js
@@ -1,15 +1,25 @@
+import axios from 'axios'
 import {Link} from 'react-router-dom'
+
+
 
 const Restaurant = (props) => {
 	const {name, review, rating, id, foodId} = props
-  return(
+	
+	const deleteRestaurant = async (foodId, id) => {
+    let res = await axios.delete(`/api/foods/${foodId}/restaurants/${id}`)
+		window.location.reload()
+		console.log (foodId)
+  }
+	
+	return(
     <div>
       <h1>Restaurant Page</h1>
 			<p>{name}:{id}</p>
 			<p>{review}</p>
 			<p>{rating}</p>
 			<div>
-			<button>Delete</button>
+			<button color='red' onClick={() => deleteRestaurant(foodId, id)}>Delete</button>
 			<Link to={`/foods/${foodId}/restaurants/${id}/update`}>
 			<button>Update</button>
 			</Link>
